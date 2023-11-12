5e44e1e44798483e56f7978a959285e4c3a3a8e1
devsantosgabriel <euinstrutorgabriel@gmail.com>
2023-11-12 16:03:25 -0300

    initial: commit initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment ={
+    firebase :{
+        apiKey: "AIzaSyDlAOdZue_OEiPH-VdnfwrcgYTVPrzIdlM",
+        authDomain: "chatfirebase-f46f5.firebaseapp.com",
+        databaseURL: "https://chatfirebase-f46f5-default-rtdb.firebaseio.com",
+        projectId: "chatfirebase-f46f5",
+        storageBucket: "chatfirebase-f46f5.appspot.com",
+        messagingSenderId: "880569101839",
+        appId: "1:880569101839:web:2db48bca80f882ff4dfd7a"
+      },
+      production: false
+}
